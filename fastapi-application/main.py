03b572f1cb81eb3dd2bdf25c4b798362d5f36337
Mikhail <lobodamisha29@gmail.com>
2025-04-01 17:37:41 +0200

    Created docker-compose.yml

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -9,4 +9,9 @@ app.include_router(api_router,
                    )
 
 if __name__ == '__main__':
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(
+        app="main:app",
+        host=settings.api.host,
+        port=settings.api.port,
+        reload=True
+    )
